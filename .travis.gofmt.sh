83fd5900b0b2fad962bf8c2a6cdcaaaa78da1ca6
Shiqing <shiqing-thu18@yandex.com>
2019-11-06 18:08:50 +0800

    Revert "[ci skip] Update Travis configuration"
    This reverts commit 15a6ea1761d80ee7cf7a94c654ccb2800f9d461a.

--- .travis.gofmt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Taken from https://github.com/google/gopacket/blob/master/.travis.gofmt.sh
-
-cd "$(dirname $0)"
-if [ -n "$(go fmt ./...)" ]; then
-  echo "Go code is not formatted, run 'go fmt ./...'" >&2
-  exit 1
-fi
