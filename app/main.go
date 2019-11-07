2cc5ed691b77f741fd5dbacd806c4627cf01b9db
Shiqing <shiqing-thu18@yandex.com>
2019-11-08 01:44:21 +0800

    Add database integration

--- app/main.go
+++ app/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 )
 
 const HTTPListenPort = 3434
+
+var db *sql.DB
+
+var schema = `
+CREATE TABLE visitor (
+	name TEXT,
+	count INTEGER
+)`
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the home page!")
@@ -15,10 +25,44 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 func nameHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	fmt.Fprintf(w, "Hi %s!", vars["name"])
+	name := vars["name"]
+	var count int
+	row := db.QueryRow("SELECT count FROM visitor WHERE name = $1", name)
+	err := row.Scan(&count)
+	if err == sql.ErrNoRows {
+		_, err = db.Exec("INSERT INTO visitor(name, count) VALUES ($1, 1)", name)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		count = 1
+	} else if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	} else {
+		count += 1
+		_, err = db.Exec("UPDATE visitor SET count = $1 WHERE name = $2", count, name)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+	}
+	fmt.Fprintf(w, "Hi %s, your #%d visit!", name, count)
 }
 
 func main() {
+	// $ initdb -D ./data
+	// $ pg_ctl -D ./data start
+	// $ createdb dbqwq -U uwu
+	var err error
+	db, err = sql.Open("postgres", "sslmode=disable dbname=dbqwq user=uwu")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
+	db.Exec(schema)
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", rootHandler)
 	r.HandleFunc("/{name:[a-z]+}", nameHandler)
