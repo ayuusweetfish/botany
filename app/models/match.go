9b7d3318fd5c77e6d0f9ae508096818694ec1ea9
list17 <1511500409@qq.com>
2019-12-18 16:27:23 +0800

    fix the match list

--- app/models/match.go
+++ app/models/match.go
@@ -120,45 +120,49 @@ func (m *Match) Read() error {
 }
 
 func ReadByContest(cid int32) ([]Match, error) {
-	rows, err := db.Query("SELECT match.id, match.status, match.report, "+
-		"contest.title, s.id, s.created_at, s.status, s.language, "+
-		"u.id, u.handle, u.nickname, u.privilege "+
-		"FROM match LEFT JOIN contest ON contest.id = match.contest "+
-		"LEFT JOIN match_party ON match_party.match = match.id "+
-		"LEFT JOIN submission s on s.id = match_party.submission "+
-		"LEFT JOIN users u on s.uid = u.id "+
+	rows, err := db.Query("SELECT DISTINCT match.id, match.status, match.report, "+
+		"c.id, c.title, c.banner, c.owner, c.start_time, c.end_time, c.descr, c.details, c.is_visible, c.is_reg_open, c.script, "+
+		"submission.id, submission.uid, submission.contest, submission.created_at, submission.status, submission.message, submission.language, submission.contents, "+
+		"u.id, u.handle, u.email, u.password, u.privilege, u.joined_at, u.nickname, u.bio "+
+		"FROM match "+
+		"JOIN contest c on match.contest = c.id "+
+		"JOIN match_party ON match.id = match_party.match "+
+		"JOIN submission ON match_party.submission = submission.id "+
+		"JOIN users u on submission.uid = u.id "+
 		"WHERE match.contest = $1 ORDER BY match.id DESC", cid)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 	ms := []Match{}
-	var mid int32 = -1
-	mlen := 0
 	for rows.Next() {
 		m := Match{Contest: cid}
-		c := Contest{Id: cid}
-		s := Submission{Contest: cid}
+		s := Submission{}
 		u := User{}
-		err := rows.Scan(&m.Id, &m.Status, &m.Report, &c.Title,
-			&s.Id, &s.CreatedAt, &s.Status, &s.Language,
-			&u.Id, &u.Handle, &u.Nickname, &u.Privilege)
+		err := rows.Scan(&m.Id, &m.Status, &m.Report, &m.Rel.Contest.Id, &m.Rel.Contest.Title, &m.Rel.Contest.Banner, &m.Rel.Contest.Owner, &m.Rel.Contest.StartTime,
+			&m.Rel.Contest.EndTime, &m.Rel.Contest.Desc, &m.Rel.Contest.Details, &m.Rel.Contest.IsVisible, &m.Rel.Contest.IsRegOpen, &m.Rel.Contest.Script,
+			&s.Id, &s.User, &s.Contest, &s.CreatedAt, &s.Status, &s.Message, &s.Language, &s.Contents,
+			&u.Id, &u.Handle, &u.Email, &u.Password, &u.Privilege, &u.JoinedAt, &u.Nickname, &u.Bio)
 		if err != nil {
 			return nil, err
 		}
-		s.Rel.User = u
-		if m.Id != mid || mid == -1 {
-			mid = m.Id
-			m.Rel.Contest = c
+		if len(ms) == 0 {
+			s.Rel.User = u
+			s.Rel.Contest = m.Rel.Contest
 			m.Rel.Parties = append(m.Rel.Parties, s)
 			ms = append(ms, m)
-			mlen += 1
 		} else {
-			ms[mlen-1].Rel.Parties = append(ms[mlen-1].Rel.Parties, s)
+			if m.Id == ms[len(ms)-1].Id {
+				s.Rel.User = u
+				s.Rel.Contest = m.Rel.Contest
+				ms[len(ms)-1].Rel.Parties = append(ms[len(ms)-1].Rel.Parties, s)
+			} else {
+				s.Rel.User = u
+				s.Rel.Contest = m.Rel.Contest
+				m.Rel.Parties = append(m.Rel.Parties, s)
+				ms = append(ms, m)
+			}
 		}
-		// TODO: Optimize
-		// m.LoadRel()
-		// ms = append(ms, m)
 	}
 	return ms, rows.Err()
 }
