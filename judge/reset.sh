492f40acdbdc550189ad9dc0ace4f2b2eb64a852
Shiqing <shiqing-thu18@yandex.com>
2020-01-09 16:26:55 +0800

    judge: Fix reset.sh fails without lib/ in chroot

--- judge/reset.sh
+++ judge/reset.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo cp compile.sh match.sh $1/var/botany
-sudo cp lib/* $1/var/botany/lib
+sudo cp -r lib $1/var/botany/lib
 sudo rm -rf $1/var/botany/matches/*
 sudo rm -rf $1/var/botany/submissions/*
