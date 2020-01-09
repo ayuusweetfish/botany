73d74d2d6d5f7de07b6c9b17bb8e1b1e3bfe2d83
Shiqing <shiqing-thu18@yandex.com>
2020-01-09 20:00:30 +0800

    Fix incorrectly spelled wildcard in reset.sh

--- judge/reset.sh
+++ judge/reset.sh
@@ -2,5 +2,5 @@
 
 sudo cp compile.sh match.sh $1/var/botany
 sudo cp -r lib $1/var/botany/lib
-find $1/var/botany/matches -type d -name '12*' -exec rm -rf {} \;
-find $1/var/botany/submissions -type d -name '12*' -exec rm -rf {} \;
+find $1/var/botany/matches -type d -name '*' -exec rm -rf {} \;
+find $1/var/botany/submissions -type d -name '*' -exec rm -rf {} \;
