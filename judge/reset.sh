4ebfed6fe53465e9a9e82986082ae02e49264ce0
Shiqing <shiqing-thu18@yandex.com>
2020-01-15 12:03:25 +0800

    [ci skip] judge: Update reset.sh for complete reset

--- judge/reset.sh
+++ judge/reset.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 sudo cp compile.sh match.sh $1/var/botany
+sudo rm -rf $1/var/botany/lib
 sudo cp -r lib $1/var/botany/lib
-find $1/var/botany/matches -type d -name '*' -exec rm -rf {} \;
 find $1/var/botany/submissions -type d -name '*' -exec rm -rf {} \;
+find $1/var/botany/matches -type d -name '*' -exec rm -rf {} \;
+sudo mkdir -p $1/var/botany/submissions
+sudo chown 1000 $1/var/botany/submissions
+sudo chgrp 1000 $1/var/botany/submissions
+sudo mkdir -p $1/var/botany/matches
+sudo chown 1000 $1/var/botany/matches
+sudo chgrp 1000 $1/var/botany/matches
