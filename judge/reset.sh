b2b49d33936366077a4f3517a970a88eadf7abad
Shiqing <shiqing-thu18@yandex.com>
2020-03-03 23:49:43 +0800

    [ci skip] judge: Fix reset.sh for copying lib/

--- judge/reset.sh
+++ judge/reset.sh
@@ -3,10 +3,11 @@
 sudo rm -rf $1/var/botany
 sudo mkdir $1/var/botany
 sudo cp compile.sh match.sh $1/var/botany
-sudo cp -r lib $1/var/botany/lib
-sudo mkdir -p $1/var/botany/submissions
+sudo mkdir $1/var/botany/lib
+sudo cp lib/* $1/var/botany/lib
+sudo mkdir $1/var/botany/submissions
 sudo chown `whoami` $1/var/botany/submissions
 sudo chgrp `whoami` $1/var/botany/submissions
-sudo mkdir -p $1/var/botany/matches
+sudo mkdir $1/var/botany/matches
 sudo chown `whoami` $1/var/botany/matches
 sudo chgrp `whoami` $1/var/botany/matches
